d9bf2f0277c555815e0ade8e3f2ef2e9346c26a5
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2021-11-27 12:51:15 +0100

    installed styled components and created flashcard wrapper

--- src/Containers/NavBar/NavBar.js
+++ src/Containers/NavBar/NavBar.js
@@ -1,9 +1,27 @@
-function NavBar({ logo, user }) {
+import styled from 'styled-components';
+
+const NavBarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: solid;
+  height: 5vh;
+`;
+
+const NavBarLogo = styled.div`
+  margin-left: 3vw;
+`;
+
+const NavBarUser = styled.div`
+  margin-right: 3vw;
+`;
+
+function NavBar({ logoImg, logoText, user }) {
     return (
-        <div className="navbar">
-            <div className="navbar__logo">{logo}</div>
-            <div className="navbar__user">Logged in as: {user}</div>
-        </div>
+        <NavBarWrapper>
+            <NavBarLogo>{logoImg} {logoText}</NavBarLogo>
+            <NavBarUser>Logged in as: {user}</NavBarUser>
+        </NavBarWrapper>
     );
 }
 

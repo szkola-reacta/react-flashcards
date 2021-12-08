d143aa19fe05fff0f7b89e17adf067882d5da1cf
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2021-12-08 19:17:24 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import './App.css';
 import MainView from './Containers/MainView/MainView';
-import NavBar from './Containers/NavBar/NavBar';
+import SignInView from './Containers/SignInView/SignInView';
+import WelcomeView from './Containers/WelcomeView/WelcomeView';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <MainView />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/react-flashcards/app" element={<MainView />} />
+          <Route path="/react-flashcards/sign-in" element={<SignInView />} />
+          <Route path="/react-flashcards/" element={<WelcomeView />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Containers/SignInView/SignInView.js
@@ -0,0 +1,5 @@
+function SignInView() {
+    return <div>Sign in view</div>;
+}
+
+export default SignInView;

1de8edbbd8ba8605198fbcc398360e36fcbf2b58
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2021-12-09 00:11:37 +0100

    commented out the lines of code preventing the app from building

--- src/Containers/SignInView/SignInView.js
+++ src/Containers/SignInView/SignInView.js
@@ -1,14 +1,14 @@
-import { useNavigate, useLocation } from "react-router-dom";
+// import { useNavigate, useLocation } from "react-router-dom";
 
 function SignInView() {
 
     // those lines are needed to redirect a user to the page they were trying to reach
     // before being asked to log in
-    const navigate = useNavigate();
-    const { state } = useLocation();
-    const handleLogin = () => {
-        navigate(state?.path || "/react-flashcards/");
-    };
+    // const navigate = useNavigate();
+    // const { state } = useLocation();
+    // const handleLogin = () => {
+    //     navigate(state?.path || "/react-flashcards/");
+    // };
 
     return <div>Sign in view</div>;
 }

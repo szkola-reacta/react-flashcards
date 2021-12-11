515e98d4a4e374a8e5abe4ac4a8cb04741c1912b
MJanowskiDev <mat.krz.janowski@gmail.com>
2021-12-11 20:34:04 +0100

    Added expand/collapse functionality

--- src/Components/Drawer/Drawer.js
+++ src/Components/Drawer/Drawer.js
@@ -1,41 +1,59 @@
-import styled from 'styled-components';
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
+
+const textColor = 'white';
+const backgroundColor = '#16191d';
+const border = '#61DAFB';
 
 const Container = styled.div`
-    padding: 0;
-    margin-top: 5vh;
+    margin-top: calc(5vh + 6px);
     height: 100%;
     display: flex;
     position: absolute;
     width: 300px;
     left: calc(100% - 300px);
     top: 0;
-    background: white;
+    background-color: ${backgroundColor};
+    transform: translateX(280px);
+    transition: transform 150ms ease-in-out;
+    ${(props) => props.expanded && css`transform: translateX(0px);`};
 `;
 
 const DrawerResizeLine = styled.div`
-    width: 5px;
-    background: red;
+    width: 2px;
+    background: ${border};
 `;
 
 const Chevron = styled.button`
     width: 32px;
     height: 32px;
-    background: red;
+    background: white;
+    border: 2px solid ${border};
+    color: ${backgroundColor};
+    font-weight: 700;
     position: absolute;
-    left: -16px;
+    left: -15px;
     top: 100px;
+    border-radius: 50%;
+}
 `;
 
 const Content = styled.div`
     padding: 16px;
-    background: white;
+    color: ${textColor};
 `;
 
 const Drawer = ({ children }) => {
+    const [ expanded, setExpanded ] = useState(false);
+
+    const handleChevronClick = () => {
+        setExpanded((prev) => !prev);
+    };
+
     return (
-        <Container>
+        <Container expanded={expanded}>
             <DrawerResizeLine />
-            <Chevron />
+            <Chevron onClick={handleChevronClick}>{expanded ? '>' : '<'}</Chevron>
             <Content>{children}</Content>
         </Container>
     );

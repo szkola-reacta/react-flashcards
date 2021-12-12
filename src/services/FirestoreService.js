30699212e5080c60fa6adfadddd5897101cbe43d
lumat18 <lumat@op.pl>
2021-12-12 11:26:45 +0100

    RF-35 fix directory name

--- src/services/FirestoreService.js
+++ src/services/FirestoreService.js
@@ -1,5 +1,5 @@
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { getRandomInt } from '../utils/helpers';
+import { getRandomInt } from '../UTILS/helpers';
 
 import { db } from './firebase';
 

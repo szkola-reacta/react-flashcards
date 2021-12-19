5e0eeb8bebf543eb66e553a07d5c1f7de2f3f675
lumat18 <lumat@op.pl>
2021-12-19 19:36:43 +0100

    RF 35 UTILS -> utils

--- src/services/FirestoreService.js
+++ src/services/FirestoreService.js
@@ -1,5 +1,5 @@
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import { getRandomInt } from '../UTILS/helpers';
+import { getRandomInt } from '../utils/helpers';
 
 import { db } from './firebase';
 
